eb62d49a5015df5c07467b9cf5b0e06c0ee4dcb3
Drew Wells <drew.wells00@gmail.com>
2015-11-24 16:46:03 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+)
+
+var ep string
+var timeout time.Duration
+
+// maximum allowed parallel HTTP threads
+var max int = 10
+
+func init() {
+	flag.StringVar(&ep, "ep", "", "endpoint for testing ie. localhost")
+	flag.DurationVar(&timeout, "timeout", 3*time.Second, "Time to run ie. 3s")
+}
+
+func main() {
+	flag.Parse()
+
+	process(ep, timeout, max)
+}
+
+type work struct {
+	req *http.Request
+	cli *http.Client
+}
+
+func (w *work) Do() Result {
+	now := time.Now()
+	resp, err := w.cli.Do(w.req)
+	if err != nil {
+		return Result{err: err}
+	}
+	// bs, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(bs))
+	defer resp.Body.Close()
+
+	return Result{
+		Code: uint16(resp.StatusCode),
+		clen: resp.ContentLength,
+		dur:  time.Since(now),
+	}
+}
+
+type Result struct {
+	err  error
+	clen int64
+	dur  time.Duration
+
+	Code        uint16
+	FanDuration []int64
+	ReadTime    time.Duration
+	Server      string
+	ReadTimes   string
+}
+
+type manager struct {
+	todo chan work
+	// Responses with 200
+	fin chan Result
+	// Responses that errored
+	err     chan Result
+	closing chan *struct{}
+	// Notify when last worker completed
+	last  chan *struct{}
+	done  chan *struct{}
+	limit int
+
+	res    []Result
+	errres []Result
+}
+
+func process(ep string, dur time.Duration, limit int) {
+	m := &manager{
+		todo: make(chan work),
+		fin:  make(chan Result),
+		err:  make(chan Result),
+
+		last:    make(chan *struct{}),
+		done:    make(chan *struct{}),
+		closing: make(chan *struct{}),
+	}
+
+	go m.collect()
+	go m.load(ep)
+	go m.workers(limit)
+
+	<-time.After(dur)
+	close(m.closing)
+
+	<-m.done
+	report(m.res, "final", false)
+	if len(m.errres) > 0 {
+		sortErrors(m.errres)
+	}
+	return
+
+}
+
+func sortErrors(rs []Result) {
+	m := make(map[string]int)
+	for _, r := range rs {
+		e := r.err.Error()
+		if _, ok := m[e]; !ok {
+			m[e] = 0
+		}
+		m[e] = m[e] + 1
+	}
+	fmt.Println("Errors reported:")
+	for k, i := range m {
+		fmt.Printf("(%d) %s\n", i, k)
+	}
+}
+
+func (m *manager) collect() {
+	results := make([]Result, 0, 10000)
+	errs := make([]Result, 0, 1000)
+	for {
+		select {
+		case r := <-m.fin:
+			results = append(results, r)
+		case r := <-m.err:
+			errs = append(errs, r)
+		case <-m.last:
+			m.res = results
+			m.errres = errs
+			close(m.done)
+			return
+		}
+	}
+}
+
+func (m *manager) load(ep string) error {
+	_, err := http.NewRequest("GET", ep, nil)
+	if err != nil {
+		// Practicie request, if it fails do nothing
+		return err
+	}
+	for {
+		// Requests can't be reused, add one to each worker
+		req, _ := http.NewRequest("GET", ep, nil)
+		w := work{
+			req: req,
+			cli: http.DefaultClient,
+		}
+		select {
+		case <-m.closing:
+			return nil
+		case m.todo <- w:
+		}
+	}
+}
+
+func (m *manager) workers(lim int) {
+	mu := sync.Mutex{}
+	c := sync.NewCond(&mu)
+
+	wg := sync.WaitGroup{}
+
+	var cur int
+	defer func() {
+		// Before closing, wait for all workers to finish
+		wg.Done()
+		fmt.Println("DONE!")
+		close(m.last)
+	}()
+
+	go func() {
+		for {
+			select {
+			case w := <-m.todo:
+				mu.Lock()
+				for cur == lim {
+					c.Wait()
+				}
+				cur = cur + 1
+				mu.Unlock()
+				wg.Add(1)
+				go func() {
+					res := w.Do()
+
+					mu.Lock()
+					cur = cur - 1
+					mu.Unlock()
+
+					if res.err == nil {
+						m.fin <- res
+					} else {
+						m.err <- res
+					}
+					wg.Done()
+					c.Signal()
+				}()
+
+			}
+		}
+	}()
+
+	select {
+	case <-m.closing:
+		fmt.Println("returned")
+		return
+	}
+
+}
---
+++ reporter.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+	"time"
+)
+
+type Results []Result
+
+func (r Results) Sort() Results {
+	sort.Sort(r)
+	return r
+}
+
+func (r Results) Len() int           { return len(r) }
+func (r Results) Less(i, j int) bool { return r[i].dur < r[j].dur }
+func (r Results) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+
+func report(results []Result, name string, displayFan bool) {
+	calc := NewMetrics(results)
+	calc.Sort()
+
+	_25m := calc.GetPercentile(0.25)
+	_75m := calc.GetPercentile(0.75)
+	_95m := calc.GetPercentile(0.95)
+	_99m := calc.GetPercentile(0.99)
+	_999m := calc.GetPercentile(0.999)
+
+	var fan FanMetrics
+	var _25f time.Duration
+	var _75f time.Duration
+	var _95f time.Duration
+	var _99f time.Duration
+	var _999f time.Duration
+	var _minf time.Duration
+	var _maxf time.Duration
+	var _meanf float64
+	var _stddevf float64
+	if displayFan {
+		fan = NewFanMetrics(results)
+		fan.Sort()
+		_25f = fan.GetPercentile(0.25)
+		_75f = fan.GetPercentile(0.75)
+		_95f = fan.GetPercentile(0.95)
+		_99f = fan.GetPercentile(0.99)
+		_999f = fan.GetPercentile(0.999)
+		_minf = fan.Min
+		_maxf = fan.Max
+		_stddevf = fan.StdDev()
+		_meanf = fan.Mean
+	}
+
+	fmt.Println(strings.Repeat("=", 30))
+	fmt.Println("Results -", name)
+	fmt.Println(strings.Repeat("-", 30))
+	fmt.Println("Requests:", calc.Total)
+	if displayFan {
+		fmt.Println("FanRequests:", fan.Total)
+	}
+
+	fmt.Println("Latencies:")
+	fmt.Printf("\tTotal:     %s\n", calc.TotalRtt)
+	if displayFan {
+		fmt.Printf("\tTotalFan:  %s\n\n", fan.TotalRtt)
+	}
+
+	fmt.Printf("\t     \t   Client")
+	fmt.Printf("\t    Fan\n")
+	fmt.Printf("\t\t" + strings.Repeat("-", 12))
+	fmt.Printf("\t" + strings.Repeat("-", 12) + "\n")
+
+	fmt.Printf("\t0.25:\t%s\t%s\n", _25m, _25f)
+	fmt.Printf("\t0.75:\t%s\t%s\n", _75m, _75f)
+	fmt.Printf("\t0.95:\t%s\t%s\n", _95m, _95f)
+	fmt.Printf("\t0.99:\t%s\t%s\n", _99m, _99f)
+	fmt.Printf("\t0.999:\t%s\t%s\n\n", _999m, _999f)
+
+	fmt.Printf("\tmean:\t%s\t%s\n", time.Duration(calc.Mean), time.Duration(_meanf))
+	fmt.Printf("\tstd:\t%s\t%s\n", time.Duration(calc.StdDev()), time.Duration(_stddevf))
+	fmt.Printf("\tmin:\t%s\t%s\n", calc.Min, _minf)
+	fmt.Printf("\tmax:\t%s\t%s\n", calc.Max, _maxf)
+	fmt.Printf("\tiqr:\t%s\t%s\n", time.Duration(_75m-_25m), time.Duration(_75f-_25f))
+
+	fmt.Println(strings.Repeat("=", 30))
+}
+
+func dumpToFile(results []Result) {
+	f, err := os.Create("results.txt")
+	check(err)
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	for _, result := range results {
+		_, err := f.WriteString(fmt.Sprintf("%d,%s\n",
+			result.dur.Nanoseconds(), result.Server))
+		check(err)
+	}
+}
+
+func dumpFanToFile(results []Result) {
+	f, err := os.Create("fan-results.txt")
+	check(err)
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	for _, result := range results {
+		durations := make([]string, len(result.FanDuration))
+		for i := 0; i < len(result.FanDuration); i++ {
+			durations[i] = fmt.Sprintf("%d", result.FanDuration[i])
+		}
+		_, err := f.WriteString(strings.Join(durations, ",") + "\n")
+		check(err)
+	}
+}
+
+func dumpReadToFile(results []Result) {
+	f, err := os.Create("results-read.txt")
+	check(err)
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	var duration time.Duration
+
+	for _, result := range results {
+		times := strings.Split(result.ReadTimes, ",")
+		durations := make([]string, len(times))
+
+		for i := 0; i < len(times); i++ {
+			duration, err = time.ParseDuration(times[i])
+			if err != nil {
+				fmt.Println(err)
+				fmt.Println("index:", i, times[i])
+				duration = time.Duration(0)
+			}
+			durations[i] = fmt.Sprintf("%d", duration.Nanoseconds())
+		}
+
+		_, err := f.WriteString(strings.Join(durations, ",") + "\n")
+		check(err)
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
